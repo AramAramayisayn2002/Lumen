bf309fbcb3efe21c14b4c98ed99be47115d83a3d
AramAramayisyan2002 <aram.aramayisyan466@gmail.com>
2024-06-03 01:28:58 +0400

    update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('structure')
+    <!DOCTYPE html>
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <title>Lumen</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- CSS
+    ================================================== -->
+    @vite(['resources/css/app.css'])
+    @vite(['resources/css/bootstrap.css'])
+    @vite(['resources/css/icons.css'])
+</head>
+<body>
+<p>aaaad</p>
+</body>
+@endsection
